0126f53186fbc808993186c00b246ba152747258
Chris von Krogh <cvonkrogh@paypal.com>
2014-11-18 12:44:55 -0800

    deriving keys, k_plus done, C0 D0 not

--- des/DES.py
+++ des/DES.py
@@ -1,5 +1,6 @@
-import string
-import re
+import Hexadecimal
+import DESEncryptValidator
+import DESKey
 
 def des_encrypt (block, key):
     """
@@ -10,97 +11,19 @@ def des_encrypt (block, key):
     """
 
     # validation
-    DESEncryptValidator().validate(block, key)
+    DESEncryptValidator.DESEncryptValidator().validate(block, key)
 
-    
+    unused = DESKey.DESKey(key)
     
     return
 
-class Hexadecimal():
-    # and, or, ^
+def L (block_bit_tuple):
+    if not len(block_bit_tuple) == 64:
+        raise ValueError("L: block needs to be 64 bits")
+    return block_bit_tuple[0:32]
 
-    MAPPING = {
-        # only uses uppercase hex
-        '0' : (False, False, False, False),
-        '1' : (False, False, False, True ),
-        '2' : (False, False, True , False),
-        '3' : (False, False, True , True ),
-        '4' : (False, True , False, False),
-        '5' : (False, True , False, True ),
-        '6' : (False, True , True , False),
-        '7' : (False, True , True , True ),
-        '8' : (True , False, False, False),
-        '9' : (True , False, False, True ),
-        'A' : (True , False, True , False),
-        'B' : (True , False, True , True ),
-        'C' : (True , True , False, False),
-        'D' : (True , True , False, True ),
-        'E' : (True , True , True , False),
-        'F' : (True , True , True , True ),
-        }
+def R (block_bit_tuple):
+    if not len(block_bit_tuple) == 64:
+        raise ValueError("R: block needs to be 64 bits")
+    return block_bit_tuple[32:64]
 
-    def bit_tuple_to_hex_string(self, bit_tuple):
-        # validation
-        if not isinstance(bit_tuple, tuple):
-            raise ValueError
-        for should_be_a_bool in bit_tuple:
-            if not isinstance(should_be_a_bool, bool):
-                raise ValueError
-        length = len(bit_tuple)
-        newlength = (length / 4) * 4
-        if not length == newlength:
-            raise ValueError
-        hex_string = ''
-
-        while len(bit_tuple) > 0:
-            bite = bit_tuple[0:4]
-            bit_tuple = bit_tuple[4:]
-            for k in self.MAPPING.keys():
-                if self.MAPPING[k] == bite:
-                    hex_string = hex_string + k
-                    break
-        return hex_string
-
-    def hex_string_to_bit_tuple(self, hex_string):
-        if not DESEncryptValidator().is_hex_string(hex_string):
-            raise ValueError
-        hex_string_uppercase = hex_string.upper()
-        bit_tuple = ()
-
-        for i in range(0, len(hex_string_uppercase)):
-            bit_tuple = bit_tuple + self.MAPPING[hex_string_uppercase[i]]
-
-        return bit_tuple
-
-class DESEncryptValidator():
-    def is_hex_string(self, s):
-        m = re.search('^[0-9a-fA-F]*$', s)
-        return not m is None
-    def validate(self, block, key):
-        if not self.is_hex_string(block):
-            raise ValueError("des_encrypt: supplied block is not hex format")
-        if not self.is_hex_string(key):
-            raise ValueError("des_encrypt: supplied key is not hex format")
-        if not len(block) == 16:
-            raise ValueError("des_encrypt: supplied block must be length 16 (64 bits)")
-        if not len(key) == 16:
-            raise ValueError("des_encrypt: supplied key must be length 16 (64 bits)")
-
-        # key validation
-        bit_tuple = Hexadecimal().hex_string_to_bit_tuple(key)
-        if not len(bit_tuple) == 64:
-            raise ValueError("really this should not happen")
-        # every byte should have an odd number of bits
-        while len(bit_tuple) > 0:
-            bite = bit_tuple[0:8]
-            bit_tuple = bit_tuple[8:]
-            num_true = 0
-            for bit in bite:
-                if bit:
-                    num_true = num_true + 1
-            if not (num_true & 1) == 1:
-                bite_string = Hexadecimal().bit_tuple_to_hex_string(bite)
-                raise ValueError("odd bits required, key=%s, bite=%s" % (key, bite_string))
-
-        pass
-    pass
--- des/DESTest.py
+++ des/DESTest.py
@@ -1,9 +1,62 @@
 import DES
+import Hexadecimal
+import DESEncryptValidator
+import DESKey
+
 import unittest
 
 class DESTest(unittest.TestCase):
 
     def setUp(self):
+        pass
+
+    def test_deskey_kplus_mapping_3(self):
+        # http://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
+
+        tests = (
+            # hex encoded key, expected true bit (25-1 indicates 25th bit, zero indexed)
+            ("0101010101010110", 25-1), # 60th bit maps to 25th
+            ("0101010101011001", 26-1), # 52 -> 26
+            ("0101010101010801", 46-1), # 53 -> 46
+            )
+
+        for test in tests:
+            keystring = test[0]
+            expected_true_bit = test[1]
+            key = DESKey.DESKey(keystring)
+            for i in range(0, len(key.k_plus)):
+                if i == expected_true_bit:
+                    self.assertTrue(key.k_plus[i], msg='single bit expected true')
+                else:
+                    self.assertFalse(key.k_plus[i], msg='all other bits expected false')
+                
+    def test_deskey_kplus_mapping_2(self):
+        # all zeros (only odd parity bits set)
+        key = DESKey.DESKey("0101010101010101")
+        for boolean in key.k_plus:
+            self.assertFalse(boolean, "should be all zeros")
+
+    def test_deskey_kplus_mapping(self):
+        key = DESKey.DESKey("0E329232EA6D0D73")
+        # no repeated values, no multiples of 8
+        testing_list = []
+        for value in key.K_PLUS_MAPPING_TUPLE:
+            self.assertFalse(value in testing_list, msg='no repeated values: %s' % value)
+            testing_list.append(value)
+        should_not_be_there_list = [8, 16, 24, 32, 40, 48, 56, 64]
+        for should_not_be_there in should_not_be_there_list:
+            self.assertFalse(should_not_be_there in testing_list, msg='multiples of 8 should not be there')
+        self.assertTrue(len(key.K_PLUS_MAPPING_TUPLE) == 56, msg="length 1")
+        self.assertTrue(len(testing_list) == 56, msg="length 2")
+        for i in range(1, 64+1):
+            if (i % 8) == 0:
+                self.assertFalse(i in key.K_PLUS_MAPPING_TUPLE, msg="does not contain 1")
+                self.assertFalse(i in testing_list, msg="does not contain 2")
+            else:
+                self.assertTrue(i in key.K_PLUS_MAPPING_TUPLE, msg="contains 1")
+                self.assertTrue(i in testing_list, msg="contains 2")
+                pass
+            pass
         pass
 
     PRECOMPUTED_ENCRYPTION_VALUES = [
@@ -21,7 +74,7 @@ class DESTest(unittest.TestCase):
             [(False, True, False, False), '4'],
             ]
 
-        hex = DES.Hexadecimal()
+        hex = Hexadecimal.Hexadecimal()
         for test in tests:
             self.assertEqual(hex.bit_tuple_to_hex_string(test[0]), test[1])
 
@@ -44,7 +97,7 @@ class DESTest(unittest.TestCase):
             ['a', (True, False, True, False)],
             ['0A3', (False, False, False, False, True, False, True, False, False, False, True, True)],
             ]
-        hex = DES.Hexadecimal()
+        hex = Hexadecimal.Hexadecimal()
         for test in tests:
             self.assertEqual(hex.hex_string_to_bit_tuple(test[0]), test[1])
 
@@ -63,11 +116,11 @@ class DESTest(unittest.TestCase):
         pass
 
     def test_is_hex_string(self):
-        validator = DES.DESEncryptValidator()
-        assert validator.is_hex_string("abc") == True
-        assert validator.is_hex_string("abcx") == False
-        assert validator.is_hex_string("AbC0239428398432") == True
-        assert validator.is_hex_string("") == True
+        hex = Hexadecimal.Hexadecimal()
+        assert hex.is_hex_string("abc") == True
+        assert hex.is_hex_string("abcx") == False
+        assert hex.is_hex_string("AbC0239428398432") == True
+        assert hex.is_hex_string("") == True
 
     def test_des_encrypt_validation(self):
         happy_tests = [
